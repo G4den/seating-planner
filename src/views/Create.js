f2d2c6d6f8138d79808e118885bd369af2e9d20b
Daniel Hougaard <dani250g@hotmail.com>
2022-05-09 16:59:24 +0200

    Can now press 'Enter' to add student

--- src/views/Create.js
+++ src/views/Create.js
@@ -1,9 +1,9 @@
 import React from "react"
 import CreateStudentCard from "../components/CreateStudentCard";
 import useState from 'react-usestateref';
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { CreateRoom, CreateClassRoom } from './../actions';
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const Create = (props) => {
     const [students, setStudents, currentStudents] = useState([])
@@ -11,7 +11,7 @@ const Create = (props) => {
     const navigate = useNavigate();
 
     const addStudent = () => {
-        if(currentName.length===0)
+        if (currentName.length === 0)
             return
 
         setStudents([...students, currentName]);
@@ -23,13 +23,19 @@ const Create = (props) => {
         setStudents(currentStudents.current.filter(student => studentToRemove !== student))
     }
 
-    return(
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            addStudent();
+        }
+    }
+
+    return (
         <div className={"w-screen h-screen flex flex-col items-center justify-center"}>
             <div className={"flex flex-col space-y-8 items-center"}>
                 <div className={"flex flex-col space-y-5 items-center"}>
                     <h1 className={"text-5xl font-bold"}>Add students</h1>
                     <div className={"flex  justify-between items-end space-x-2"}>
-                        <input autoFocus={true} spellCheck={false} type={"name"} value={currentName} onChange={e => setCurrentName(e.target.value)} className={"w-72 h-14 rounded-xl text-2xl outline-none border-2 border-black text-center  "}/>
+                        <input autoFocus={true} onKeyPress={handleKeyPress} spellCheck={false} type={"name"} value={currentName} onChange={e => setCurrentName(e.target.value)} className={"w-72 h-14 rounded-xl text-2xl outline-none border-2 border-black text-center  "} />
                         <button onClick={addStudent} className={"w-16 h-14 border-2 border-black rounded-xl text-xl"}>Add</button>
                     </div>
                 </div>
@@ -39,7 +45,7 @@ const Create = (props) => {
                         <div className={"flex flex-col w-96 space-y-2 max-h-96"}>
                             <h1 className={"font-bold"}>Students</h1>
                             <div className={"grid grid-cols-3 w-full h-full overflow-auto gap-x-2 gap-y-2"}>
-                                {students.map(student => <CreateStudentCard key={student} removeStudent={removeStudent} student={student}/>)}
+                                {students.map(student => <CreateStudentCard key={student} removeStudent={removeStudent} student={student} />)}
                             </div>
                         </div>
                         <div className={"flex w-full justify-end"}>
@@ -59,4 +65,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, {CreateRoom, CreateClassRoom})(Create)
+export default connect(mapStateToProps, { CreateRoom, CreateClassRoom })(Create)
